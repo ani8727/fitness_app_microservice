38d9e9fcec3cf7e08e05be60faff3e73818bed9c
ani8727 <aniketgupta.8727@gmail.com>
2025-10-03 21:55:48 +0530

    Adding Frontend server

---
+++ gateway/src/main/java/com/fitness/gateway/user/UserService.java
@@ -0,0 +1,37 @@
+package com.fitness.gateway.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+
+    private final WebClient userServiceWebClient;
+
+    public boolean validateUser(String userId) {
+        log.info("Calling User Validation API for userId: {}", userId);
+        try {
+            return userServiceWebClient.get()
+                    .uri("/api/users/{userId}/validate", userId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new RuntimeException("User Not Found: " + userId);
+            } else if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                throw new RuntimeException("Invalid Request: " + userId);
+            }
+            throw new RuntimeException("Error calling User Service: " + e.getStatusCode(), e);
+        } catch (WebClientException e) {
+            throw new RuntimeException("Failed to connect to User Service", e);
+        }
+    }
+}
---
+++ gateway/src/main/java/com/fitness/gateway/user/WebClientConfig.java
@@ -0,0 +1,24 @@
+package com.fitness.activityservice.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    // Builder must be LoadBalanced to resolve service names via Eureka
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder
+                .baseUrl("http://USER-SERVICE") // Eureka service ID (case-insensitive)
+                .build();
+    }
+}
